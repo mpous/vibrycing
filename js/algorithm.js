a2ab8cc1da2fbe2da21a6fed374a737fb6c19d71
djkram <marc.planaguma@gmail.com>
2013-02-28 13:42:57 +0100

    alerts ok

--- js/algorithm.js
+++ js/algorithm.js
@@ -5,9 +5,9 @@
 function renderRoute(route) {
 
 	var min_dist = 0.05; // 50m
-	var distance = route.routes.legs.distance;
-	var duration = route.routes.legs.duration;
-	var steps = route.routes.legs.steps;
+	var distance = route.routes[0].legs[0].distance;
+	var duration = route.routes[0].legs[0].duration;
+	var steps = route.routes[0].legs[0].steps;
 
 	/*
  	* {
@@ -35,39 +35,67 @@ function renderRoute(route) {
  	}
  	*/
  	
- 	function isNear(){
-
-			navigator.geolocation.getCurrentPosition(succes, error);
-	
-			function error(msg){
+ 	function isNear(actual_step){
+ 		
+ 			function errorGPS(msg){
 				alert("GPS ERROR!! w00t!");
 			}
 			
-			function success(position){
+			function successGPS(position){
 				lat_now = position.coords.latitude;
 				lon_now = position.coords.longitude;
+				
+				//lat_end = actual_step.end_location.lat;
+				//lon_end = actual_step.end_location.lng;
+				
+				lat_end = lat_now;
+				lon_end = lon_now;
+				
+				/** Converts numeric degrees to radians */
+				if (typeof(Number.prototype.toRad) === "undefined") {
+				  Number.prototype.toRad = function() {
+				    return this * Math.PI / 180;
+				  }
+				}
+				
+				// Calculate distance
+				var R = 6371; // km
+				var dLat = (lat_end-lat_now).toRad();
+				var dLon = (lon_end-lon_now).toRad();
+				var lat1 = lat_now.toRad();
+				var lat2 = lat_end.toRad();
+				
+				var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+				        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+				var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+				var d = R * c;
+				
+				if (d <= min_dist){
+					
+						direction = actual_step.html_instructions;
+				
+						if (direction.indexOf("dreta") >= 0 || direction.indexOf("right") >= 0){
+							alert("dreta");
+						}
+						else if (direction.indexOf("esquerra") >= 0 || direction.indexOf("left") >= 0){
+							alert("esquerra");
+						}else{
+							alert("random!")
+						}
+					
+					return true
+				}else{
+					return false
+				}
+				
 			}
-	
-			lat_end = actual_step.end_location.lat;
-			lon_end = actual_step.end_location.lng;
+
+			setInterval(function(){
+				navigator.geolocation.getCurrentPosition(successGPS, errorGPS);
+				},5000);
 			
-			// Calculate distance
-			var R = 6371; // km
-			var dLat = (lat_end-lat_now).toRad();
-			var dLon = (lon_end-lon_now).toRad();
-			var lat1 = lat_now.toRad();
-			var lat2 = lat_end.toRad();
-			
-			var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-			        Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
-			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-			var d = R * c;
-			
-			if (d <= min_dist){
-				return true
-			}else{
-				return false
-			}
+
+
 	}	
 
 	for (var i=0;i<steps.length;i++) {
@@ -79,22 +107,16 @@ function renderRoute(route) {
 
 		do{
 			
-			near = isNear();
-			
-			if(near){
-				direction = actual_step.html_instructions;
-				
-				if (direction.indexOf("dreta") >= 0 || direction.indexOf("right") >= 0){
-					alert("dreta");
-				}
-				else if (direction.indexOf("esquerra") >= 0 || direction.indexOf("left") >= 0){
-					alert("esquerra");
-				}
-					
-			}
+			near = isNear(actual_step);
 			
 		} while (near);
+		
+		if(i==steps.length){
+			alert("The END!")
+		}
 
 	}
+	
+	
 
 }
